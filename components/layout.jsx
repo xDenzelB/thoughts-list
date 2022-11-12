c4f0aca5887740f0f290a32759eb983321d4ace3
Denzel <dbartolaba123@gmail.com>
2022-11-11 16:10:41 -0800

    added in some google fonts

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,9 +1,10 @@
 import Nav from './Nav';
 
-export default function Layout() {
+export default function Layout({children}) {
   return (
     <div>
       <Nav />
+      <main>{children}</main>
     </div>
   )
 }
