2263bb8c968c99a0c819b598b2468597cdc79c8e
Denzel <dbartolaba123@gmail.com>
2022-11-11 16:42:37 -0800

    Added in firebase for backend

--- components/layout.jsx
+++ components/layout.jsx
@@ -2,7 +2,7 @@ import Nav from './Nav';
 
 export default function Layout({children}) {
   return (
-    <div>
+    <div className="mx-6 md:max-w-2x1 md:mx-auto font-railway">
       <Nav />
       <main>{children}</main>
     </div>
