e4381ec5232a8668ad60158cb6cbad9b74e9b900
Denzel <dbartolaba123@gmail.com>
2022-11-14 15:31:32 -0800

    created component for dark mode

---
+++ components/ThemeSwitcher.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { HiOutlineSun } from 'react-icons/hi';
+import { MdOutlineDarkMode } from 'react-icons/md';
+
+export default function ThemeSwitcher() {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  const themeIcons = () => {
+    if (resolvedTheme === 'dark') {
+      return <HiOutlineSun className="text-xl md:text-2x1" />
+    } else if (resolvedTheme === 'light') {
+      <MdOutlineDarkMode className="text-xl md:text-2x1" />
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className="flex items-center justify-center w-10 h-10 mx-auto transition-all bg-gray-200 rounded-lg md:w-12 md:h-12 dark:bg-gray-600 hover:ring-2 ring-gray-300"
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}>
+      {themeIcons()}
+    </button>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
 import { ToastContainer } from 'react-toastify';
+import { ThemeProvider } from 'next-themes'
 import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <ToastContainer limit={1} />
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider>
+      <Layout>
+        <ToastContainer limit={1} />
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
